0f8e83367538b4e537b92e50e29e541aa65ce708
Christopher Hamilton <chrish001@live.com.au>
2023-06-04 20:33:32 +0930

    fix: corrects issues with versions

--- RemootioAndroidAuto/automotive/src/main/java/com/example/remootioandroidws/RemootioScreen.java
+++ RemootioAndroidAuto/automotive/src/main/java/com/example/remootioandroidws/RemootioScreen.java
@@ -25,7 +25,7 @@ public class RemootioScreen extends Screen {
         ItemList radioList =
                 new ItemList.Builder()
                         .addItem( new Row.Builder()
-                                .setTitle("Test!")
+                                .setTitle("Testing")
                                 .addText("click on any option to see toast notification")
                                 .build())
                         .addItem(new Row.Builder().setTitle("Option 2").build())
