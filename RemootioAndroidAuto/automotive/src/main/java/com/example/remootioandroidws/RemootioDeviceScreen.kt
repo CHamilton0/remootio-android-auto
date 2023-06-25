e39cbe10bb9be4f7c5b49e50876e3bf17b2569d0
Christopher Hamilton <chrish001@live.com.au>
2023-06-25 19:05:01 +0930

    feat: add second view for app

--- RemootioAndroidAuto/automotive/src/main/java/com/example/remootioandroidws/RemootioDeviceScreen.kt
+++ RemootioAndroidAuto/automotive/src/main/java/com/example/remootioandroidws/RemootioDeviceScreen.kt
@@ -5,21 +5,21 @@ import androidx.car.app.CarToast
 import androidx.car.app.Screen
 import androidx.car.app.model.*
 
-class RemootioScreen(carContext: CarContext?) : Screen(carContext!!) {
+class RemootioDeviceScreen(carContext: CarContext?) : Screen(carContext!!) {
     override fun onGetTemplate(): Template {
         val templateBuilder = ListTemplate.Builder()
         val radioList = ItemList.Builder()
-            .addItem(Row.Builder().setTitle("Garage Door").build())
-            .addItem(Row.Builder().setTitle("Gate").build())
+            .addItem(Row.Builder().setTitle("test").build())
             .setOnSelectedListener { index: Int -> onSelected(index) }
             .build()
         templateBuilder.addSectionedList(
-            SectionedItemList.create(radioList, "Remootio Devices")
+            SectionedItemList.create(radioList, "Test")
         )
-        return templateBuilder.setTitle("Remootio").build()
+        return templateBuilder.setTitle("2").setHeaderAction(Action.BACK).build()
     }
 
     private fun onSelected(index: Int) {
-        this.screenManager.push(RemootioDeviceScreen(carContext))
+        CarToast.makeText(carContext, "Changed selection to index: $index", CarToast.LENGTH_LONG)
+            .show()
     }
 }
