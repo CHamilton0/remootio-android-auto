9aff9c0c6523efb4ac4f5077770948c76c162bfe
Christopher Hamilton <chrish001@live.com.au>
2024-01-14 14:47:00 +1030

    feat(android): hide keys for doors

--- RemootioAndroidAuto/main/src/main/java/chamilton0/remootioandroidws/SettingsActivity.kt
+++ RemootioAndroidAuto/main/src/main/java/chamilton0/remootioandroidws/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package chamilton0.remootioandroidws
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
@@ -10,9 +13,7 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
         if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
+            supportFragmentManager.beginTransaction().replace(R.id.settings, SettingsFragment())
                 .commit()
         }
     }
@@ -20,6 +21,37 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val garageApiAuthKeyPreference = findPreference<EditTextPreference>("garageApiAuthKey")
+            val garageApiSecretKeyPreference =
+                findPreference<EditTextPreference>("garageApiSecretKey")
+            val gateApiAuthKeyPreference = findPreference<EditTextPreference>("gateApiAuthKey")
+            val gateApiSecretKeyPreference = findPreference<EditTextPreference>("gateApiSecretKey")
+
+            hideFieldData(garageApiAuthKeyPreference)
+            hideFieldData(garageApiSecretKeyPreference)
+            hideFieldData(gateApiAuthKeyPreference)
+            hideFieldData(gateApiSecretKeyPreference)
+        }
+
+        fun hideFieldData(field: EditTextPreference?) {
+            field?.setOnBindEditTextListener { editText ->
+                editText.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            }
+            field?.summaryProvider = PasswordSummaryProvider()
+        }
+
+        // Custom SummaryProvider to hide the password in the summary
+        private class PasswordSummaryProvider : Preference.SummaryProvider<EditTextPreference> {
+
+            override fun provideSummary(preference: EditTextPreference): CharSequence? {
+                return if (preference.text.isNullOrEmpty()) {
+                    ""
+                } else {
+                    "*".repeat(16)
+                }
+            }
         }
     }
 }
