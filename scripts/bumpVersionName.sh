2f20b0a343c2f573fff63d85eedf2a5cc844a509
Christopher Hamilton <chrish001@live.com.au>
2025-04-10 20:01:42 +0930

    chore: always allow version name bump

--- scripts/bumpVersionName.sh
+++ scripts/bumpVersionName.sh
@@ -14,39 +14,31 @@ if [ -e "$version_file" ]; then
     # Get the current branch name
     git fetch origin
 
-    # Check the Git history for keywords indicating changes
-    if git log --oneline --no-merges $(git tag | tail -n 1)..HEAD | grep -q 'BREAKING CHANGE\|feat\|fix'; then
-        # At least one of the keywords is found in the Git history
-        # Increment the version components
-        IFS='.' read -ra version_parts <<< "$current_version"
-        major_version="${version_parts[0]}"
-        minor_version="${version_parts[1]}"
-        patch_version="${version_parts[2]}"
-        
-        if git log --oneline --no-merges $(git tag | tail -n 1)..HEAD --grep='BREAKING CHANGE' | grep -q 'BREAKING CHANGE'; then
-            # Increment major version for breaking changes
-            ((major_version++))
-            minor_version=0
-            patch_version=0
-        elif git log --oneline --no-merges $(git tag | tail -n 1)..HEAD --grep='feat' | grep -q 'feat'; then
-            # Increment minor version for features
-            ((minor_version++))
-            patch_version=0
-        else
-            # Increment patch version for fixes
-            ((patch_version++))
-        fi
-
-        new_version="$major_version.$minor_version.$patch_version"
-        # Update the version in the file
-        sed -i -E 's|versionName "[0-9]+\.[0-9]+\.[0-9]+"|versionName "'"$new_version"'"|' "$version_file"
-        echo "Updated version to $new_version"
-        exit 0
+    # Increment the version components
+    IFS='.' read -ra version_parts <<< "$current_version"
+    major_version="${version_parts[0]}"
+    minor_version="${version_parts[1]}"
+    patch_version="${version_parts[2]}"
+    
+    if git log --oneline --no-merges $(git tag | tail -n 1)..HEAD --grep='BREAKING CHANGE' | grep -q 'BREAKING CHANGE'; then
+        # Increment major version for breaking changes
+        ((major_version++))
+        minor_version=0
+        patch_version=0
+    elif git log --oneline --no-merges $(git tag | tail -n 1)..HEAD --grep='feat' | grep -q 'feat'; then
+        # Increment minor version for features
+        ((minor_version++))
+        patch_version=0
     else
-        # No changes found, no version increment
-        echo "No changes found, version remains $current_version"
-        exit 1
+        # Increment patch version for fixes
+        ((patch_version++))
     fi
+
+    new_version="$major_version.$minor_version.$patch_version"
+    # Update the version in the file
+    sed -i -E 's|versionName "[0-9]+\.[0-9]+\.[0-9]+"|versionName "'"$new_version"'"|' "$version_file"
+    echo "Updated version to $new_version"
+    exit 0
 else
     echo "Error: $version_file not found."
     exit 2
